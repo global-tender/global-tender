75ebca32e6dea4c95879f174881e3d83b544b33a
Igor Popov <ihptru@gmail.com>
2015-12-22 11:31:22 +0000

    seminar apply form fixes and improvements

--- tender/views.py
+++ tender/views.py
@@ -14,6 +14,9 @@ from django.views.decorators.csrf import csrf_exempt
 from django.utils import timezone
 from django.core import mail
 
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
+
 from tender.models import FZs, Seminars, Seminar_Programs, Cities
 
 
@@ -79,7 +82,7 @@ def seminar_detail(request, arg):
 	template_args = {
 		'content': 'pages/seminar.html',
 		'request': request,
-		'title': '%s %s.%s.%s - %s' % (seminar.event_city.name, seminar.event_date.day, seminar.event_date.month, seminar.event_date.year, seminar.event_fz.name),
+		'title': '%s %s.%s.%s - %s' % (seminar.event_city.name, seminar.event_date.strftime('%d'), seminar.event_date.strftime('%m'), seminar.event_date.year, seminar.event_fz.name),
 		'menu_color_class': 'menu-white',
 		'menu_inner': 'menu-inner',
 
@@ -93,6 +96,7 @@ def seminar_detail(request, arg):
 @xframe_options_exempt
 @csrf_exempt
 def ajax_seminar(request, arg):
+	send_copy_email = ''
 
 	seminar = Seminars.objects.filter(id=arg)
 	if seminar:
@@ -102,16 +106,92 @@ def ajax_seminar(request, arg):
 	if request.method == 'POST':
 		submitted = True
 
-		form_contact_email = request.POST.get('contact-email', '')
+		############################################################
+		send_copy_email = request.POST.get('send-copy-email', '')
+		if send_copy_email == "yes":
+			send_copy_email_to = unicode(request.POST.get('contact-email',''))
+			try:
+				validate_email(send_copy_email_to)
+			except ValidationError as e:
+				send_copy_email = ''
+
+
+		form_uri = request.POST.get('uri', '')
+		if ( form_uri != '' ):
+			form_uri = u" - совпадает с юридическим "
+
+		form_act = request.POST.get('act', '')
+		form_act_type = ''
+		if ( form_act != '' ):
+			if ( form_act == 'ustav' ):
+				form_act_type = u" устава "
+			if ( form_act == 'polozhenie' ):
+				form_act_type = u" положения "
+
+		form_pol = request.POST.get('act', '')
+		if ( form_pol != '' ):
+			form_pol = " положения "
+
+		body_head = u"Семинар: %s  %s.%s.%s\n\n" % (unicode(seminar.event_city.name), seminar.event_date.strftime('%d'), seminar.event_date.strftime('%m'), seminar.event_date.year)
+		body = u"""
+		1. Организация: %s \n
+			Название организации (краткое): %s \n
+			ИНН: %s \n
+			КПП: %s \n\n
+			Почтовый адрес: %s, %s, %s, %s, %s  %s \n\n
+		2. Руководитель (для оформления договора): %s \n
+			Должность: %s \n
+			Руководитель действует на основании: %s \n
+		3. Количество участников семинара: %s \n
+		4. Контактные лицо: %s \n
+			Должность: %s \n
+			email: %s \n
+			Телефон: %s \n
+		5. Комментарий: %s \n
+		""" % (unicode(request.POST.get('org-name','')),
+				unicode(request.POST.get('org-short','')),
+				unicode(request.POST.get('inn','')),
+				unicode(request.POST.get('kpp','')),
+				unicode(request.POST.get('zip-code','')),
+				unicode(request.POST.get('region','')),
+				unicode(request.POST.get('city','')),
+				unicode(request.POST.get('street','')),
+				unicode(request.POST.get('home','')),
+				unicode(form_uri),
+				unicode(request.POST.get('fio','')),
+				unicode(request.POST.get('post','')),
+				unicode(form_act_type),
+				unicode(request.POST.get('amount','')),
+				unicode(request.POST.get('contact-name','')),
+				unicode(request.POST.get('contact-post','')),
+				unicode(request.POST.get('contact-email','')),
+				unicode(request.POST.get('contact-phone','')),
+				unicode(request.POST['comment'])
+		)
+		############################################################
+		
+		subject = u'Заявка на семинар: %s %s.%s.%s' % (seminar.event_city.name, seminar.event_date.strftime('%d'), seminar.event_date.strftime('%m'), seminar.event_date.year)
 
 
 		connection = mail.get_connection()
 		connection.open()
-		body = u"пусто, email: %s" % (form_contact_email)
-		subject = u'Заявка "Глобал-Тендер": %s %s.%s.%s' % (seminar.event_city.name, seminar.event_date.day, seminar.event_date.month, seminar.event_date.year)
-		email = mail.EmailMessage(subject, body, 'info@ihptru.net',
-						  ['ihptru@yandex.ru'], headers = {'Reply-To': 'info@ihptru.net'}, connection=connection)
+		
+		email = mail.EmailMessage(subject, body_head + body, settings.ADMIN_EMAIL_FROM,
+			settings.ADMIN_EMAIL_TO, headers = {'Reply-To': settings.ADMIN_EMAIL_FROM}, connection=connection)
+
 		email.send()
+
+
+		if send_copy_email == "yes":
+			subject = u'Вами отправлена заявка на семинар: %s %s.%s.%s' % (seminar.event_city.name, seminar.event_date.strftime('%d'), seminar.event_date.strftime('%m'), seminar.event_date.year)
+			body = u'Заявка на семинар отправлена, ожидайте обработки.\n\nСодержимое заявки:\n\n' + body
+
+			email = mail.EmailMessage(subject, body_head + body, settings.ADMIN_EMAIL_FROM,
+				[send_copy_email_to], headers = {'Reply-To': settings.ADMIN_EMAIL_FROM}, connection=connection)
+
+			email.send()
+
+
 		connection.close()
 
 
@@ -120,6 +200,7 @@ def ajax_seminar(request, arg):
 		'request': request,
 		'seminar': seminar,
 		'submitted': submitted,
+		'send_copy_email': send_copy_email,
 	}
 	context = RequestContext(request, template_args)
 	return StreamingHttpResponse(template.render(context))
