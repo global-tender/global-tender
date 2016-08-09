173ae81690bd8b34e923d2dd0d21c08386d9af39
ihptru <ihptru@gmail.com>
2016-08-09 22:00:41 +0300

    fixes

--- tender/admin.py
+++ tender/admin.py
@@ -5,9 +5,11 @@ from tender.models import Cities
 from tender.models import Seminar_Programs
 from tender.models import Seminars
 from tender.models import Banners
+from tender.models import Clients
 
 admin.site.register(FZs)
 admin.site.register(Cities)
 admin.site.register(Seminar_Programs)
 admin.site.register(Seminars)
 admin.site.register(Banners)
+admin.site.register(Clients)
