cef574b59daab3bfa66b5b7a85a48cb4e2d5808d
ihptru <ihptru@gmail.com>
2016-10-04 14:36:12 +0300

    subscribe page fixes

--- tender/sitemaps.py
+++ tender/sitemaps.py
@@ -20,7 +20,7 @@ class StaticViewSitemap(sitemaps.Sitemap):
 	protocol = 'https'
 
 	def items(self):
-		return ['index', 'seminars', 'feedback', 'contacts', 'service']
+		return ['index', 'seminars', 'feedback', 'contacts', 'service', 'subscribe']
 
 	def location(self, item):
 		if item == 'index':
