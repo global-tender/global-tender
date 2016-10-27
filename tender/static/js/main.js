d5dc1dfc82a9ef9d89bab077c11e8b32cbaa1496
ihptru <ihptru@gmail.com>
2016-10-27 16:22:31 +0300

    subscribe

--- tender/static/js/main.js
+++ tender/static/js/main.js
@@ -13,6 +13,20 @@
 	}
 /*	code: end */
 
+/*	code: select text with one click */
+	function selectText(containerid) {
+		if (document.selection) {
+			var range = document.body.createTextRange();
+			range.moveToElementText(document.getElementById(containerid));
+			range.select();
+		} else if (window.getSelection) {
+			var range = document.createRange();
+			range.selectNode(document.getElementById(containerid));
+			window.getSelection().addRange(range);
+		}
+	}
+/*	code: end */
+
 
 $(document).ready(function() {
 
